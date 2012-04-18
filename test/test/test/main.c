2d04f43bb31477a71adddf027ae3b86de7696612
Zening Qu <quzening@gmail.com>
2012-04-17 23:25:40 -0700

    just the lib

---
+++ test/test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Zening Qu on 4/17/12.
+//  Copyright (c) 2012 REMAP/UCLA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
